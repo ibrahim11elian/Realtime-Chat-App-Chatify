b0fc6ef16d4656f65bab790d78251d1ef637fb1f
ibrahim11elian <ibrahim11elian@gmail.com>
2024-08-19 19:17:20 +0300

    update the nav links in mobile devices

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('chat')" :active="request()->routeIs('chat')">
+                        {{ __('Chat') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
